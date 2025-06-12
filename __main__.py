399f8818e60f0190acd6d6b3a257090802f1cf6a
Luis H F Ramires <luishenriquefr@outlook.com>
2025-06-11 23:12:05 -0300

    finally configured new machine

--- __main__.py
+++ __main__.py
@@ -1,7 +1,7 @@
 import pymysql
 import os
 import boto3
-import time
+import json
 from functions.conta import alimenta_cartao_threaded, alimenta_transacao_threaded, alimenta_banco_conta_threaded
 from functions.pessoa import alimenta_banco_pessoa_threaded
 from functions.investimentos import alimenta_banco_investimento_threaded, alimenta_banco_ordens_threaded
@@ -13,22 +13,24 @@ access_key = os.getenv("AWS_ACCESS_KEY")
 secret_key = os.getenv("AWS_ACCESS_KEY_SECRET")
 db_endpoint = os.getenv("DB_ENDPOINT")
 db_port = int(os.getenv("DB_PORT"))
-db_user = os.getenv("DB_USER")
-db_password = os.getenv("DB_PASSWORD")
+db_secret_key = os.getenv("DB_SECRET_KEY")
 db_region = 'sa-east-1'
 db_name = os.getenv("DB_NAME")
 bucket_name = os.getenv("BUCKET_NAME")
 os.environ['LIBMYSQL_ENABLE_CLEARTEXT_PLUGIN'] = '1'
 
-session = boto3.Session(profile_name='default',
-                        region_name='sa-east-1')
+session = boto3.Session(aws_access_key_id=access_key
+                        , aws_secret_access_key=secret_key
+                        , region_name=db_region
+                        )
 rds = session.client("rds")
-s3 = session.client("s3"
-                    ,aws_access_key_id=access_key
-                    ,aws_secret_access_key=secret_key
-                    ,region_name=db_region)
+s3 = session.client("s3")
+secrets_manager = session.client("secretsmanager")
+value_key = secrets_manager.get_secret_value(SecretId=db_secret_key)
+db_login = json.loads(value_key['SecretString'])
 
-
+db_user = db_login['username']
+db_password = db_login['password']
 
 token = rds.generate_db_auth_token(DBHostname=db_endpoint
                                 ,Port=db_port
@@ -39,9 +41,9 @@ token = rds.generate_db_auth_token(DBHostname=db_endpoint
 try:
     con_params = {
         'host': db_endpoint,
+        'port': db_port,
         'user': db_user,
         'password': db_password,
-        'port': db_port,
         'database': db_name,
         'auth_plugin_map': {'mysql_clear_password': None},
         'connect_timeout': 30,
@@ -55,7 +57,7 @@ try:
     cursor.execute("SELECT COUNT(*) FROM TRANSACOES")
     query = cursor.fetchall()
     print(query[0])
-
+    pass
 
 except Exception as e:
     print(e)
